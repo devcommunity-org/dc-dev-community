13a6a4c2e0488206ea1ac229ceb50e09eb9d3797
Jared Alexander <jaredalexander@users.noreply.github.com>
2020-04-19 10:12:21 -0400

    Updating welcome message

--- lib/localization.dart
+++ lib/localization.dart
@@ -17,7 +17,7 @@ class MyLocalizations {
       'pageTitle': 'DC Dev Community',
       "aboutUs": "About Us",
       "volunteerToSpeak": "Volunteer to Speak",
-      "homeBodyText": "Welcome to DC Dev Community! We are a family of meetups that span a wide variety of tech disciplines, including mobile, web, and cloud development. Check out our upcoming meetups, and volunteer to speak - we are always looking for fresh faces and ideas!",
+      "homeBodyText": "Welcome to Dev Community! We are a family of meetups that span a wide variety of tech disciplines, including mobile, web, and cloud development. Check out our upcoming meetups, and volunteer to speak - we are always looking for fresh faces and ideas!",
       "upcomingMeetups": "Upcoming Meetups",
       "featuredVideo": "Featured Video",
       "volunteerToSpeak": "Volunteer to Speak"
